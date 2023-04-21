d00fc9ca05d79d29dcc6bddb7765c6e0513543af
santrami <santrami@hotmail.com>
2023-04-21 22:22:25 +0200

    adding render.ts

--- render.ts
+++ render.ts
@@ -1,59 +1,42 @@
-import { User } from "./users.js";
+import { Curiosity } from "./Curiosity.js";
+/* import { loadPhotos } from "./photos.js"; */
 
-const head = (title: string) => `
-<head>
+const head = `<head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>${title}</title>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-    }
-    .user {
-      font-family: sans-serif;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: .4rem;
-      border-bottom: 1px solid #ddd;
-    }
-    .user img {
-      width: 3rem;
-      height: 3rem;
-      border-radius: 50%;
-      margin-right: 0.7rem;
-    }
-    .user .name {
-      font-weight: bold;
-    }
-    .user .email {
-      font-family: monospace;
-    }
-  </style>
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>`;
 
-const renderUsers = (users: Array<User>) => {
+
+const renderPhotos = (photos: Array<Curiosity>) => {
   let html = "";
-  for (const user of users) {
-    html += `<div class="user">
-      <img src="${user.picture.medium}" />
-      <div class="data">
-        <div class="name">${user.fullName}</div>
-        <div class="email">${user.email}</div>
-      </div>
-    </div>`;
+  for (const photo of photos) {
+    html += `      
+        <div class="item">
+          <h3 class="title">tomada con la cámara ${photo.camera.name}</h3>     
+          <img src=" ${photo.image} "/>
+          <a href="./individuales/${photo.id}.html" class="btn">Ver más detalles</a>    
+        </div>    
+    `;
   }
   return html;
-}
+};
 
-export const render = (users: Array<User>) => {
+
+
+export const render = (photos: Array<Curiosity>) => {
   return `
-<html>
-  ${head("User List")}
+<!DOCTYPE html>
+${head}
   <body>
-    ${renderUsers(users)}
+    <div class="container">
+      ${renderPhotos(photos)}
+    </div>
   </body>
 </html>`;
 };
+
+//const photos= await loadPhotos();
+/* renderPhotos(photos); */
